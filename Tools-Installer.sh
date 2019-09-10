9e63f6c44b0505afacf4655f17537b33cd6c47f7
Official Toxic Cyber Team <52749659+ToxicCyberTeam@users.noreply.github.com>
2019-09-10 18:49:25 +0700

    Add files via upload

---
+++ Tools-Installer.sh
@@ -0,0 +1,304 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+clear
+echo
+sleep 1
+clear
+echo
+echo $cy"<=========================================================>"
+echo $pu"Github   : https://github.com/Makky2693"
+echo $pu"WhatsApp : +92 313-1694897"
+echo $pu"Facebook : https://m.facebook.com/Makky"
+echo $pu"YouTube  : http://youtube.com/c/Makky2693"
+echo $cy"<=========================================================>"
+echo
+echo $i"MENU PILIHAN:"
+echo
+echo $cy"["$me"1"$cy"]"$i" REPORT FB"
+echo $cy"["$me"2"$cy"]"$i" DARK FB"
+echo $cy"["$me"3"$cy"]"$i" HACK Wi-Fi (Root)"
+echo $cy"["$me"4"$cy"]"$i" ADMIN FINDER"
+echo $cy"["$me"5"$cy"]"$i" CLONING YAHOO"
+echo $cy"["$me"6"$cy"]"$i" OSIF"
+echo $cy"["$me"7"$cy"]"$i" PROFILEGUARD FB"
+echo $cy"["$me"8"$cy"]"$i" BOT REACTION FACEBOOK"
+echo $cy"["$me"9"$cy"]"$i" SPAM CALL"
+echo $cy"["$me"10"$cy"]"$i" SPAM WHATSHAPP"
+echo $cy"["$me"11"$cy"]"$i" YOUTUBE-DL"
+echo $cy"["$me"12"$cy"]"$i" LACAK LOKASI"
+echo $cy"["$me"13"$cy"]"$i" TERMUX ART"
+echo $cy"["$me"14"$cy"]"$i" TOMBOL TERMUX"
+echo $cy"["$me"15"$cy"]"$i" DDOS"
+echo $cy"["$me"16"$cy"]"$i" DEFACE MASAL"
+echo $cy"["$me"17"$cy"]"$i" HACK KAMERA"
+echo $cy"["$me"18"$cy"]"$i" PHISING SOSMED"
+echo $cy"["$me"19"$cy"]"$i" HASH KILLER"
+echo $cy"["$me"20"$cy"]"$i" BUAT SCRIPT DEFACE"
+echo $cy"["$me"99"$cy"]"$i" ALL MEMBER OFFICIAL TCT"
+echo $pur"["$i"0"$pur"]"$ku" EXIT"
+echo
+echo $bi"╭─# Tools Installer ["$bi"Masukkan pilihan anda"$bi"]"
+read -p"╰─# Official TCT >>" pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+./wifi-hacker.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/AdheBolo/AdminFinder
+cd AdminFinder
+python2 AdminFinder.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/jaxBCD/FBshield.git
+cd FBshield
+python2 guard.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/edi-nasa12/ROBOT.git
+cd ROBOT
+python2 blackbot.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/Aditya021/SpamCall
+cd SpamCall
+php SpamCall.php
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/rahmadxyz/sub-bot.git
+cd sub-bot
+php sub-bot.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator.git
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/ToxicCyberTeam/Colour-Termux
+cd Colour-Termux
+chmod +x *
+./TermuxArt.sh
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/ToxicCyberTeam/Tombol
+cd Tombol
+python Tombol.py
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/ToxicCyberTeam/DDOS
+cd DDOS
+python Tusbol.py
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/ToxicCyberTeam/Deface-Masal
+cd Deface-Masal
+python2 Tutorial.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+sh saycheese.sh
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+sh shellphish.sh
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/FajriHidayat088/FHX-Hash-Killer
+cd FHX-Hash-Killer
+python2 FHXHashKiller.py
+fi
+
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+sleep 1
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+chmod +x create.py
+python2 create.py
+fi
+
+if [ $pil = 99 ]
+then
+clear
+figlet -f slant "T C T"|lolcat
+echo $cy"OFFICIAL TOXIC CYBER TEAM"
+echo
+echo $pu" • Mr k3mpl03"
+echo $pu" • SkeezN"
+echo $pu" • Mr Fath"
+echo $pu" • Carlos"
+echo $pu" • Mr 007"
+echo $pu" • Mr GOTAKX"
+echo $pu" • ArRose"
+echo $pu" • Mr Dhilz"
+echo $pu" • Exotic Shadow"
+echo
+echo $cy"["$me"0"$cy"]"$i" Untuk kembali"
+echo $bi"╭─# From Toxic To Brotherhood ["$bi"Kembali"$bi"]"
+read -p"╰─# Official TCT ~: " pil
+fi
+
+if [ $pil = 0 ]
+then
+clear
+sh Tools-Installer.sh
+fi
+
+
+if [ $pil = 0 ]
+then
+clear
+figlet -f slant "T C T"|Ulolcat
+sleep 2
+echo $cy"Semoga Bermanfaat"
+sleep 1
+echo $i"Jangan Lupa Share Tools Ini"
+sleep 1
+echo $pur"Dan Subscribe Channel Saya"
+sleep 1
+echo $ku"Facebook :"$i" Makky_2693"
+sleep 1
+echo $ku"WhatsApp :"$i" 083874961264"
+sleep 1
+echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
+sleep 1
+clear
+exit
+fi
